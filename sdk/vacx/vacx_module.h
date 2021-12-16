d1395d29c832567c62cfa422394f294de350167e
5H3RR1 <86661312+5H3RR1@users.noreply.github.com>
2021-12-16 23:00:52 +0300

    Create vacx_module.h

---
+++ sdk/vacx/vacx_module.h
@@ -0,0 +1,37 @@
+#include "vacx_hooked.h"
+
+long double Patch(BYTE char* src) {
+    class char256 : public WCHAR {
+        public:
+        static char256<T> FromIndex(6);
+        T*		clear() const;
+        void	write(const T* src);
+        operator T*() const;
+        bool operator!=(T *src) const;
+        const char256& operator=(const T *src);
+        T*	operator->() const;
+    };
+
+    static char256* dwthink = NexusAPI::HexialPush(NexusAPI::OverrideThink(0x0bf), strcmp(this*));
+
+    return NexusAPI::PerfectThink(dwthink, "85 C0 ? ? ? 00");
+}
+
+typedef class VACX {
+    public:
+    short	incxpatch(char T* kvpatch);
+    int kvpatch& operator=(uint32_t T *kvpatch_try);
+    kvpatch_try* operator->() uint32_t;
+};
+
+extern class __int32 {
+    public:
+    double int	incxpatch(VACX T* kvpatch);
+    kvpatch_try* operator->() new[] *VACX uintptr_t(int, uint16_t);
+};
+
+extern class __ubool {
+    public:
+    unsigned bool	incxpatch(VACX T* kvpatch);
+    kvpatch_try* operator->() new[] *VACX uintptr_t(bool, uint16_t);
+};
