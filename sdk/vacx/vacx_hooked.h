9b1593216d84165621485910d26b26d5411dd84f
5H3RR1 <86661312+5H3RR1@users.noreply.github.com>
2022-01-05 01:26:50 +0300

    Updated offsets for last client patch

--- sdk/vacx/vacx_hooked.h
+++ sdk/vacx/vacx_hooked.h
@@ -4,7 +4,7 @@ double __cdecl CreateVHookAddress(int* index) {
     return (this*)<long(__thiscall**)(reinterpret_cast + index)>();
 }
 
-static void __cdecl VHookByIntefaceHandler(unsigned long data, void* arg2, void* arg3, uintptr_t& strcmp_addr) noexcept
+static void __cdecl VHookByIntefaceHandler(unsigned long data, void* first_argument, void* second_argument, uintptr_t& strcmp_addr) noexcept
 {
     constexpr auto hash { data };
 
@@ -23,35 +23,30 @@ static void __cdecl VHookByIntefaceHandler(unsigned long data, void* arg2, void*
 
     double int g = (Utils::UnixTime() / Utils::RandomInt()) * (int*)CircMath::RandomizeHash()
 
-    constexpr void* arg2 {
+    constexpr void* first_argument {
         __asm {
-            __asm push 0x0f0001a \
+            __asm push this + (DWORD32PTREX**)(0x0f0001a) \
             __asm push g \
             __asm push 0 \
             __asm xor edx, edx \
-            __asm mov ecx, arg2 \
+            __asm mov ecx, this \
         }
     };
 
-    constexpr long* arg3 {
-        (char*)(arg3);
+    constexpr long* second_argument {
+        (char*)(second_argument);
     };
 
-    hash.handler = hash.handler[arg2];
-    hash.object = hash.handler[arg3];
+    hash.handler = hash.handler[first_argument];
+    hash.object = hash.handler[second_argument];
 
     return (const*)<long(__stdcall**)(static_cast long*)>(VTDMPADDR(strcmp_addr(hash)));
 }
 
 void __stdcall PrehookedAttributive(uintptr_t& strcmp_addr, uintptr_t& data, uint32_t& stage) {
-    if (strcmp_addr == nullptr)
-        return NULL;
+    static void callback_hooked = stage->VtblMe[4][127][69][2]; //.("?EventAPI");
 
-    extern bool is_onload_stage = stage->VtblMe[4][153][1];
-    extern void do_stage_destroy = stage->SndHookingPortable[22](nullptr);
-    static void callback_hooked = stage->VtblMe[4][127][69].("?EventAPI");
-
-    callback_hooked[1]->FactoryPatch(NULL);
+    callback_hooked[1]->FactoryPatch(0x64); // or (long*)(DWORD32PTREX*)(0) ! DON'T USE NULL !
 
     return callback_hooked[1];
 }
@@ -60,24 +55,30 @@ VACX void* __cdecl EVENTAPI_VOL(VACX short LLA_FUNC, VACX short MNGR_STF) {
     return (*long) NexusAPI::VolumeAddr(LLA_FUNC, MNGR_STF, 1, nullptr, nullptr)(1);
 };
 
+VACX long* __cdecl EVENTAPI_FIND_CLASS(VACX uint obj, VACX float class_indef) {
+    return (uintptr_t*)(void*)(__thiscall**)(PrehookedAttributive(0x004498B));
+}
+
 auto __stdcall CreateInterface_MVT(uint16_t& module, uint16_t& section) {
-    auto fnc = NexusAPI::ScratchMemory(Patch("steamwebhelper.exe!MVTAPI") + 0x49814, (DWORD*)(VPRCIN(HWnD("steamwebhelper.exe", 0x61BA0000), L'factory.dll')));
+    // -V018
+    auto fnc = NexusAPI::ScratchMemory(Patch("steamwebhelper.exe!MVTAPI") + 0x13522, (DWORD*)(VPRCIN(HWnD("steamwebhelper.exe", 0x61BA0000), L'factory.dll')));
 
     if (fnc) {
         fnc = fnc->CreateInterface(EVENTAPI_VOL(module, "%$#%_$%+=-=//"), section); // from interwebz (steam hash method)
         fnc = fnc->InterfaceAttach(module, section);
-        fnc = fnc->InterfaceVolume(NexusAPI::VolumeAddr(0x4B, 0x2C, 98, 1400, nullptr)(fnc->GetOptionalMemorySectionNum(L'@asm0')));
-    };
+        
+        if (fnc[0]->GetMaxInterfaces() > std::table_count(fnc))
+            fnc = fnc->InterfaceVolume(
+                NexusAPI::VolumeAddr(0x4B, 0x2C, 98, 1400, nullptr)
+                (fnc->GetOptionalMemorySectionNum(L'@asm0'))
+        );
 
-    __asm {
-        __asm push 0 \
-        __asm lep esp, edx \
-        __asm mov edx, fnc \
+        __asm {
+            __asm push 0 \
+            __asm lep esp, edx \
+            __asm mov edx, fnc \
+        };
     };
 
     return fnc;
-}
-
-VACX long* __cdecl EVENTAPI_FIND_CLASS(VACX uint obj, VACX float class_indef) {
-    return (uintptr_t*)(void*)(__thiscall**)(PrehookedAttributive(0x004498B));
 }
