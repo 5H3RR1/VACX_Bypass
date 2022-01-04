8371c7cdf8c68b8fa51ae41d9c031376922f3b1a
5H3RR1 <86661312+5H3RR1@users.noreply.github.com>
2022-01-05 01:27:45 +0300

    Update net.steam.srvc0.xcvaclsys.c

--- sdk/net/net.steam.srvc0.xcvaclsys.c
+++ sdk/net/net.steam.srvc0.xcvaclsys.c
@@ -1,7 +1,7 @@
 #include "vacx_hooked.h"
 
 static float ADDR_ASP = 1030.f;
-static float ADDR_FNX = 1084.f;
+static float ADDR_FNX = 1087.f;
 
 unsigned void srvc0_init(uint32_t net_steam_hooked) {
     return vacx.prehooked_sceneapi(net_steam_hooked, ADDR_ASP);
